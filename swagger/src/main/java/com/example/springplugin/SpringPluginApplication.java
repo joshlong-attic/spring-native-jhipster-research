8a0261be80ddb7d72df317d8b4e209acded880fe
Josh Long <josh@joshlong.com>
2021-09-30 19:43:06 -0700

    go

---
+++ swagger/src/main/java/com/example/springplugin/SpringPluginApplication.java
@@ -0,0 +1,53 @@
+package com.example.springplugin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.JdkProxyHint;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.plugin.core.support.PluginRegistryFactoryBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@JdkProxyHint(types = {
+	java.util.List.class,
+	org.springframework.aop.SpringProxy.class,
+	org.springframework.aop.framework.Advised.class,
+	org.springframework.core.DecoratingProxy.class
+})
+@TypeHint(
+	types = {
+		PluginRegistryFactoryBean.class
+	},
+	access = AccessBits.ALL
+)
+@SpringBootApplication
+public class SpringPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPluginApplication.class, args);
+	}
+
+}
+
+@Controller
+@ResponseBody
+class GreetingRestController {
+
+	@GetMapping("/hello/{name}")
+	Greeting greet(@PathVariable String name) {
+		return new Greeting("Hello, " + name + "!");
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Greeting {
+	private String greeting;
+}
---
+++ swagger/src/test/java/com/example/springplugin/SpringPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
